cba81196f83e6c7784cc04e68540eb642fe96513
LikemDzokoto <likemdzokoto@gmail.com>
2023-12-10 12:40:20 +0000

    update

--- packages/react-app/pages/_app.tsx
+++ packages/react-app/pages/_app.tsx
@@ -4,22 +4,19 @@ import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
 import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import {InjectedConnector} from "wagmi/connectors/injected"
 import { publicProvider } from "wagmi/providers/public";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string; // get one at https://cloud.walletconnect.com/app
+
 
 const { chains, publicClient } = configureChains(
   [Celo, Alfajores],
   [publicProvider()]
 );
 
-const connectors = celoGroups({
-  chains,
-  projectId,
-  appName: (typeof document === "object" && document.title) || "Your App Name",
-});
+const connectors ={new InjectedConnector({chains})}
 
 const appInfo = {
   appName: "Celo Composer",
